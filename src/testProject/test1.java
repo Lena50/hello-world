538096c7d185608efe7ce064d6aa003374b39b5e
lingna <dailingna50@gmail.com>
2015-12-30 10:10:23 +0800

    no comment

---
+++ src/testProject/test1.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2015 KAPPS Consulting Pte. Ltd. All Rights Reserved. 
+ * 
+ * This software is the proprietary information of KAPPS Consulting Pte. Ltd. 
+ * Use is subjected to license terms. 
+ *
+ * @since Dec 29, 2015 3:47:03 PM
+ * @author dailingna
+ * @testProject
+ *
+ * Version Control Info
+ *
+ * $Id: codetemplates.xml,v 1.1 2008/08/21 01:32:51 ryeow Exp $
+ * $Revision: 1.1 $
+ * $Date: 2008/08/21 01:32:51 $
+ */
+package testProject;
+
+public class test1
+{
+	public String aaa="";
+}
